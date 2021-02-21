3f1b6de8bd79eeb2e8aae3209989e4f6ba98c8f1
KevinTss <kevin.tassi@gmail.com>
2021-02-21 20:04:14 +0100

    add exercise

---
+++ exercices/switch.js
@@ -0,0 +1,13 @@
+/*
+Intervertir deux variables
+*/
+
+var boite_1 = 'A';
+var boite_2 = 'B';
+
+var boite_3 = boite_1;
+boite_1 = boite_2;
+boite_2 = boite_3;
+
+console.log('Boite 1: ', boite_1); // contenu de la boite 2
+console.log('Boite 2: ', boite_2); // contenu de la boite 1
