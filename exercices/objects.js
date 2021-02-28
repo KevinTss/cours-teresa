fd374e2893af4131a86cfcd214b62677adfa8b9a
KevinTss <kevin.tassi@gmail.com>
2021-02-28 19:57:39 +0100

    add exercise vairbale

---
+++ exercices/objects.js
@@ -0,0 +1,30 @@
+var var_olivier = {
+  nom: 'Olivier',
+  '3nom': 'doux',
+  age: 33,
+  adresse: {
+    rue: 'Rue du soleil',
+    numero: 4,
+    cp: 1234,
+    ville: 'Bruxelles',
+  },
+  'quelque chose': 'autroe chose',
+};
+
+var nom = 'quelque chose';
+
+var_olivier.nom; // Olivier
+var_olivier['3nom'];
+
+var_olivier['quelque chose'];
+
+var ma_voiture = {
+  marque: 'Renault',
+  modele: 'Clio',
+  année: 2002,
+  gasoline: 'Diesel',
+};
+
+ma_voiture['année'];
+
+ma_voiture.année;
