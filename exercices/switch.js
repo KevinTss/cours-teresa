85a112fee4fe68e7c74d33ace96b28083f5b5043
KevinTss <kevin.tassi@gmail.com>
2021-04-08 19:00:15 +0200

    Add conditions

--- exercices/switch.js
+++ exercices/switch.js
@@ -2,12 +2,12 @@
 Intervertir deux variables
 */
 
-var boite_1 = 'A';
-var boite_2 = 'B';
+var boite_1 = "A";
+var boite_2 = "B";
 
 var boite_3 = boite_1;
 boite_1 = boite_2;
 boite_2 = boite_3;
 
-console.log('Boite 1: ', boite_1); // contenu de la boite 2
-console.log('Boite 2: ', boite_2); // contenu de la boite 1
+console.log("Boite 1: ", boite_1); // contenu de la boite 2
+console.log("Boite 2: ", boite_2); // contenu de la boite 1
